bd09d5b86f25b196d05e9d8680a5133e67fd3b1e
anggafahrisetiawan <54840282+anggafahrisetiawan@users.noreply.github.com>
2020-08-31 20:02:16 +0700

    Add files via upload

---
+++ UTS/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+      linkedList ll = new linkedList();
+      ll.add("10");
+      
+
+      System.out.println(ll.txt());
+      ll.remove("10");
+      System.out.println(ll.txt());
+      System.out.println(ll.contains("200"));
+
+    }
+}
